f78020e27181d51f9b7b2b71c54acc0172afdec1
beeri <amitohayon232@gmail.com>
2022-11-30 05:51:06 +0200

    Card list in the making...

--- src/cmps/custom-card.vue
+++ src/cmps/custom-card.vue
@@ -1,7 +1,7 @@
 <template>
     <article class="custom-card flex column items-center">
       <header>
-        <slot name="header"></slot>
+        <slot name="card-header"></slot>
       </header>
       <main>
         <slot name="main"></slot>
@@ -11,34 +11,4 @@
       </footer>
     </article>
   </template>
-  
-  <style scoped>
-  .custom-card {
-    justify-content: space-between;
-    background: white;
-   
-    width: 20.25rem;
-    border: 1px solid black;
-    border-radius: 5px;
-  }
-  
-  .custom-card header {
-     height:12.125rem;
-     width: 100%;
-    font-size: 2rem;
-    border: 3px solid black;
-  }
-  
-  .custom-card main {
-    height: 8.75rem;
-    text-align: center;
-    background-color: blue;
-  }
-  
-  .custom-card footer {
-    height: 3.75rem;
-    font-size: 1rem;
-    color: gray;
-  }
-  </style>
   
--- src/views/gig-preview.vue
+++ src/views/gig-preview.vue
@@ -1,56 +1,76 @@
 <template>
-    <li class="gig-preview">
-      <custom-card >
-        <template #header>
-            <!-- <img class="toy-preview__img" src="@/assets/demo-toy.jpg" alt="" /> -->
+  <li class="gig-preview">
+    <custom-card>
+      <template #card-header>
+        <img class="card-img" :src="gig.image" alt="" />
+      </template>
+      <template #main>
+        <section class="card-main-header">
+          <img class="card-seller-img" :src="gig.imgUrl">
+          <span class="card-seller-online"></span>
 
-         
-        </template>
-        <template #main>
-           <p><span class="fw-bold">Name:</span> {{ gig.fullname }}</p>
-          <!-- <p><span class="fw-bold">In stock:</span> {{ gig.inStock }}</p> -->
-          <p><span class="fw-bold">Price:</span> ${{ gig.price }}</p>
-            <!-- mini user - user.img + user.name
+          <section class="card-seller-name-rating flex column">
+            <span class="card-seller-name">{{gig.fullname}}</span>
+            <span class="card-seller-level">{{gig.daysToMake}}</span>
+          </section>
+          <section class="card-seller-clients">Clients</section>
+        <!-- <p><span class="fw-bold">Name:</span> {{ gig.fullname }}</p> -->
+        <!-- <p><span class="fw-bold">In stock:</span> {{ gig.inStock }}</p> -->
+        <!-- <p><span class="fw-bold">Price:</span> ${{ gig.price }}</p> -->
+        </section>
+        <div class="card-main-header-gap"></div>
+        <section class="card-main-center">
+          <h4>{{ gig.title }}</h4>
+        </section>
+        <!-- mini user - user.img + user.name
             gig.title -->
-          <p><span class="fw-bold">Name:</span> {{ gig.title }}</p>
-          <!-- <p><span class="fw-bold">In stock:</span> {{ gig.inStock }}</p> -->
-          <p><span class="fw-bold">Price:</span> ${{ gig.price }}</p>
-        </template>
+        <section class="card-main-footer ">
+        <!-- <p><span class="fw-bold">Name:</span> {{ gig.title }}</p> -->
+        
+        </section>
+        <!-- <p><span class="fw-bold">In stock:</span> {{ gig.inStock }}</p> -->
+        <!-- <p><span class="fw-bold">Price:</span> ${{ gig.price }}</p> -->
+      </template>
+      <template #footer class="card-footer flex">
+        <img class="card-fav-hart" src="../assets/imgbin_color-paper-grey-white-png.png"/>
+        <!-- add to favorite (heart sign) / price  -->
+        <div class="btn-group">
+          <span class="starting-at">STARTING AT</span>
+          <span class="card-gig-price">{{gig.price}}</span>
+          <!-- {{this.decimelToLowerSize(gig.price)}} -->
+          <!-- <button @click="goToEdit" class="btn btn-primary">edit</button> -->
+          <!-- <button @click="goToDetail" class="btn btn-info">details</button> -->
+          <!-- <button @click="removeGig(gig._id)" class="btn btn-danger">delete</button> -->
+        </div>
+      </template>
+    </custom-card>
+  </li>
+</template>
+<script>
+import customCard from "../cmps/custom-card.vue";
 
-        <template #footer >
-            <!-- add to favorite (heart sign) / price  -->
-          <div class="btn-group">
-            <button @click="goToEdit" class="btn btn-primary">edit</button>
-            <button @click="goToDetail" class="btn btn-info">details</button>
-            <button @click="removeGig(gig._id)" class="btn btn-danger">delete</button>
-          </div>
-        </template>
-      </custom-card>
-    </li>
-  </template>
-  
-  <script>
-  import customCard from '../cmps/custom-card.vue'
-  
-  export default {
-    name: 'gig-preview',
-    props: {
-      gig: Object,
+export default {
+  name: "gig-preview",
+  props: {
+    gig: Object,
+  },
+  components: {
+    customCard,
+  },
+  methods: {
+    goToDetail() {
+      this.$router.push(`/gig/${this.gig._id}`);
     },
-    components: {
-      customCard,
+    goToEdit() {
+      this.$router.push(`/gig/edit/${this.gig._id}`);
     },
-    methods: {
-      goToDetail() {
-        this.$router.push(`/gig/${this.gig._id}`)
-      },
-      goToEdit() {
-        this.$router.push(`/gig/edit/${this.gig._id}`)
-      },
-      removeGig(gigId) {
-        this.$emit('removeGig', gigId)
-      },
+    removeGig(gigId) {
+      this.$emit("removeGig", gigId);
     },
-  }
-  </script>
-  
+    decimelToLowerSize(price) {
+      console.log(arguments)
+      return `<span class="dollar_sign">$</span><span class="dollars"><?= $part[0] ?></span>.<span class="cents"><?= $part[1] ?></span>`   
+    }
+  },
+};
+</script>
