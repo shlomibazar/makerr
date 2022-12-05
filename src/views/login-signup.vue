346e190c3fb6c8d7e6b132198c8610ce22172540
LielR <lielr10@gmail.com>
2022-12-05 15:01:41 +0200

    Fixed login modal closing after clicking

--- src/views/login-signup.vue
+++ src/views/login-signup.vue
@@ -1,54 +1,17 @@
 <template>
-  <div class="container about">
-    <p>{{ msg }}</p>
-
-    <div v-if="loggedinUser">
-      <h3>
-        Loggedin User:
-        {{ loggedinUser.fullname }}
-        <button @click="doLogout">Logout</button>
-      </h3>
-    </div>
-    <div v-else>
-      <h2>Login</h2>
-      <form @submit.prevent="doLogin">
-        <select v-model="loginCred.username">
-          <option value="">Select User</option>
-          <option v-for="user in users" :key="user._id" :value="user.username">{{ user.fullname }}</option>
-        </select>
-        <!-- <input type="text" v-model="loginCred.username" placeholder="User name" />
-        <input
-          type="text"
-          v-model="loginCred.password"
-          placeholder="Password"
-        /> -->
-        <button>Login</button>
-      </form>
-      <p class="mute">user1 or admin, pass:123 </p>
-      <form @submit.prevent="doSignup">
-        <h2>Signup</h2>
-        <input type="text" v-model="signupCred.fullname" placeholder="Your full name" />
-        <input type="text" v-model="signupCred.password" placeholder="Password" />
-        <input type="text" v-model="signupCred.username" placeholder="Username" />
-        <img-uploader @uploaded="onUploaded"></img-uploader>
-        <button>Signup</button>
-      </form>
-    </div>
-    <hr />
-    <details>
-      <summary>
-        Admin Section
-      </summary>
-      <ul>
-        <li v-for="user in users" :key="user._id">
-          <pre>{{ user }}</pre>
-          <button @click="removeUser(user._id)">x</button>
-        </li>
-      </ul>
-    </details>
-  </div>
+  <div class="login-modal">
+    <h1>Sign In to Makerr</h1>
+    <p class="mute">user1 or admin, pass:123 </p>
+    <form @submit.prevent="doSignup">
+      <h2>Sign up</h2>
+      <input type="text" v-model="signupCred.fullname" placeholder="Your full name" />
+      <input type="password" v-model="signupCred.password" placeholder="Password" />
+      <input type="text" v-model="signupCred.username" placeholder="Username" />
+      <img-uploader @uploaded="onUploaded"></img-uploader>
+      <button class="login-modal-btn" >Continue</button>
+    </form>
+</div>
 </template>
-
 <script>
 
 import imgUploader from '../cmps/img-uploader.vue'
@@ -74,6 +37,7 @@ export default {
     this.loadUsers()
   },
   methods: {
+  
     async doLogin() {
       if (!this.loginCred.username) {
         this.msg = 'Please enter username/password'
@@ -96,8 +60,8 @@ export default {
         return
       }
       await this.$store.dispatch({ type: 'signup', userCred: this.signupCred })
+      this.$emit("closeModal");
       this.$router.push('/')
-
     },
     loadUsers() {
       this.$store.dispatch({ type: "loadUsers" })
