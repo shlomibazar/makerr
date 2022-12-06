834bdd81cf82edd96a066bec97c622f692636f04
LielR <lielr10@gmail.com>
2022-12-06 11:48:49 +0200

    add gig button added

--- src/views/profile.vue
+++ src/views/profile.vue
@@ -1,25 +1,41 @@
 <template>
   <div class="profile-main-container">
     <div class="leftside-mini-profile">
-    <div class="profile-user-avatar">
-        <img src="https://pngroyale.com/wp-content/uploads/2022/05/User-Avatar-PNG-Free-Download.png">
-    </div>
-    <div class="profile-user-details">
+      <div class="profile-user-avatar">
+        <img
+          src="https://pngroyale.com/wp-content/uploads/2022/05/User-Avatar-PNG-Free-Download.png"
+        />
+      </div>
+      <div class="profile-user-details">
         <span class="profile-user-details-from">From: Israel</span>
         <span class="profile-user-details-since">Member Since: Last Week</span>
         <span class="profile-user-details-response">Avg Time Response: 5 days</span>
-    </div>
+      </div>
     </div>
     <div class="rightside-gigs-profile">
-        <div class="profile-gigs-nav">
-            <button>Active Gigs</button>
-            <button>Paused Gigs</button>
-        </div>
-        <!-- <div class="rightside-gigs-list">
-            X X
-        </div> -->
+      <div class="profile-gigs-nav">
+        <button>Active Gigs</button>
+        <button>Paused Gigs</button>
+      </div>
+      <div class="addgig-section">
+        <button @click="addGigPage()">Add Gig</button>
+      </div>
     </div>
   </div>
 </template>
 
-<script></script>
+<script>
+// import imgUploader from "../cmps/img-uploader.vue";
+
+export default {
+  name: "profile",
+  data() {
+    return {};
+  },
+  methods: {
+    addGigPage() {
+      this.$router.push("/gig/edit");
+    },
+  },
+};
+</script>
