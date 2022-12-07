825937d2e1e38349240c2a676bce535971e2c15c
beeri <amitohayon232@gmail.com>
2022-12-07 21:31:45 +0200

    modified:   src/services/user.service.js

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -75,7 +75,7 @@ async function signup(userCred) {
     return saveLocalUser(user)
 }
 async function logout() {
-    // sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN_USER)
+    sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN_USER)
     socketService.logout()
     return await httpService.post('auth/logout')
 }
