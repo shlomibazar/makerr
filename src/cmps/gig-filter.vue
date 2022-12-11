93fb5d439d7609a6d8621b6bdec2600fd9e4f91b
beeri <amitohayon232@gmail.com>
2022-12-11 18:29:33 +0200

    modified:   src/assets/styles/views/gig-app.scss 	modified:   src/cmps/gig-filter.vue 	modified:   src/views/gig-app.vue

--- src/cmps/gig-filter.vue
+++ src/cmps/gig-filter.vue
@@ -1,6 +1,5 @@
 <template>
   <section class="gig-filter flex items-center filter-conteiner">
-
     <div class="flex">
       <!-- BUDGET MODAL -->
       <div class="dropdown">
@@ -8,7 +7,6 @@
           Budget
           <Transition>
           <span class="budget-arrow" :class="{ 'arrow-up': isBudgetModal }">
-          
             <svg width="11" height="7" viewBox="0 0 11 7" xmlns="http://www.w3.org/2000/svg">
               <path
                 d="M5.4636 6.38899L0.839326 1.769C0.692474 1.62109 0.692474 1.38191 0.839326 1.23399L1.45798 0.61086C1.60483 0.462945 1.84229 0.462945 1.98915 0.61086L5.72919 4.34021L9.46923 0.61086C9.61608 0.462945 9.85354 0.462945 10.0004 0.61086L10.619 1.23399C10.7659 1.38191 10.7659 1.62109 10.619 1.769L5.99477 6.38899C5.84792 6.5369 5.61046 6.5369 5.4636 6.38899Z">
@@ -40,7 +38,6 @@
           </div>
         </div>
       </div>
-
       <!-- DELIVERY TIME MODAL -->
       <div class="dropdown">
         <button class="filter-button" @click="deliveryTimeModal()">
@@ -56,17 +53,6 @@
         </button>
         <div class="delivery-time-modal" v-if="this.isDeliveryModal" v-click-outside="deliveryTimeModal">
           <section class="delivery-time-radio flex">
-            <!-- <div>
-
-          <p>Tutorials:{{ tutorials }}</p>
-          <input v-model="tutorials" type="radio" value="Data Structures" name="ds" />
-          <label for="ds">Data Structures</label>
-          <input v-model="tutorials" type="radio" value="Algorithms" name="al" />
-          <label for="al">Algorithms</label>
-          <input v-model="tutorials" type="radio" value="Machine Learning" name="ml" />
-          <label for="ml">Machine Learning</label>
-        </div> -->
-
             <div class="flex">
               <input class="radio-btn" type="radio" id="express" name="express" value="1" v-model="buttonChoose" />
               <label class="radio-label fivepix" for="express">Express 24H</label><br />
@@ -84,7 +70,6 @@
                 <label class="radio-label" for="anytime">Anytime</label><br />
             </div>
           </section>
-
           <div class="filter-approve flex">
             <div class="clear-button" @click="clearAllDellTime">Clear All</div>
             <button class="apply-button" @click="setFilterDelTime(buttonChoose)">
@@ -95,14 +80,12 @@
       </div>
     </div>
     <div class="sort-button">
-      Sort By:
-      <b>
-        <a @click="setSort('rate')">Rate</a>
+      Sort by<b>
+        <a class="rate-word" @click="setSort('rate')">Rate</a>
       </b>
     </div>
   </section>
 </template>
-
 <script>
 import { Search } from "@element-plus/icons-vue";
 import _ from "lodash";
@@ -144,12 +127,10 @@ export default {
       this.$emit("filteredDel", this.buttonChoose);
     },
     setFilterTxt() {
-      console.log("this.$store.getters.labels", this.$store.getters.labels);
       this.$emit("filteredTxt", this.txt);
     },
 
     setFilterPrice() {
-      console.log("this.minPrice", this.minPrice);
       this.$emit("filteredPrice", this.minPrice);
     },
     setFilterDelTime() {
@@ -157,7 +138,6 @@ export default {
       this.$emit("filteredDel", this.buttonChoose);
     },
     setFilterBudget() {
-      console.log("this.budgetttttttttttttt", this.budget);
 
       // if (!this.budget.minPrice || typeof this.budget.minPrice === "string") {
       if (!this.budget.minPrice ) {
@@ -165,7 +145,6 @@ export default {
       }else{
       this.budget.minPrice = +(this.budget.minPrice)
       }
-      // if (!this.budget.maxPrice || typeof this.budget.maxPrice === "string") {
       if (!this.budget.maxPrice ) {
         this.budget.maxPrice = 9999;
       }else{
@@ -174,10 +153,6 @@ export default {
       this.isBudgetModal = false;
       this.$emit("filteredBudget", this.budget);
     },
-
-    // setFilterLabel() {
-    //   this.$emit('filteredLabel', this.selectedLabels)
-    // },
     setFilterStatus() {
       this.$emit("filteredStatus", this.status);
     },
@@ -198,10 +173,6 @@ export default {
     },
   },
   computed: {
-    // labels() {
-    //   // console.log('this.$store.getters.labels',this.$store.getters.labels);
-    //   return this.$store.getters.labels
-    // },
     search() {
       return Search;
     },
